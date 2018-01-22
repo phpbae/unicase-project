4eccf89d0960434ccf537300132551cba3799129
phpbae <z1004man@naver.com>
2018-01-22 22:30:39 +0900

    파일 수정.

--- src/mainpage/Index.java
+++ src/mainpage/Index.java
@@ -1,7 +1,7 @@
 package mainpage;
 
-import DB.*;
-import bean.*;
+import DB.ItemDAO;
+import bean.ItemBean;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,137 +17,72 @@ import java.util.ArrayList;
  */
 @WebServlet("/Index")
 public class Index extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public Index() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Index() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		// 상품목록 정보를 불러오기.
+        ArrayList<ItemBean> listItem = null;
+        String site = null;
+        try {
+            request.setCharacterEncoding("utf-8");
 
-		try {
-			
-			//파라미터 한글처리
-			request.setCharacterEncoding("utf-8");
+            //파라미터 처리.
+            // 1 : 데스크탑
+            // 2 : 노트북
+            // 3 : 오디오
+            // 4 : 마우스
+            // 5 : 키보드
 
-			//파라미터 처리.
-			// 1 : 데스크탑
-			// 2 : 노트북
-			// 3 : 오디오
-			// 4 : 마우스
-			// 5 : 키보드
-			
-			
-			String search = null;
-			
-			if(request.getParameter("search") != null){
-				
-				search = request.getParameter("search");
-				
-			}
-			
-			
-			
-			System.out.println("써치");
-			System.out.println(search);
-			
-			
-			if(search == null){
-			// 검색 안했을시.	
-				
-				int goods_num = 0;
-				if(request.getParameter("goods") != null){
-					
-				goods_num =  Integer.parseInt(request.getParameter("goods"));
-				
-				}
-				
-				if(goods_num == 0){
-					
-					//첫페이지 8개 나오는거
-					ArrayList<ItemBean> listitem = ItemDAO.get_main_item_new();
+            String search = null;
+            if (request.getParameter("search") != null) {
+                search = request.getParameter("search");
+            }
+            if (search == null) {
+                int goods_num = 0;
+                if (request.getParameter("goods") != null) {
+                    goods_num = Integer.parseInt(request.getParameter("goods"));
+                }
 
-					// request에 담는다.
-					request.setAttribute("listitem", listitem);
+                if (goods_num == 0) {
+                    listItem = ItemDAO.get_main_item_new();
+                    site = "main.jsp?goods=" + goods_num;
+                } else {
+                    listItem = ItemDAO.filter_get_item(goods_num);
+                    site = "main.jsp?goods=" + goods_num;
+                }
+            } else {
+                listItem = ItemDAO.search_get_item(search);
+                site = "main.jsp?search=" + search;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-					// 페이지 이동
-					String site = "main.jsp?goods=" + goods_num;
-					RequestDispatcher dis = request.getRequestDispatcher(site);
-					dis.forward(request, response);
-					
-					
-				} else {
-					
-					ArrayList<ItemBean> listitem = ItemDAO.filter_get_item(goods_num);
+        request.setAttribute("listitem", listItem);
+        RequestDispatcher dis = request.getRequestDispatcher(site);
+        dis.forward(request, response);
+    }
 
-					// request에 담는다.
-					request.setAttribute("listitem", listitem);
-
-					// 페이지 이동
-					String site = "main.jsp?goods=" + goods_num;
-					RequestDispatcher dis = request.getRequestDispatcher(site);
-					dis.forward(request, response);
-					
-					
-				}
-				
-				
-				
-			}else{
-				
-				//검색값이 존재할때.
-				//등록된 상품이름으로 검색.
-				
-				
-				ArrayList<ItemBean> listitem = ItemDAO.search_get_item(search);
-
-				// request에 담는다.
-				request.setAttribute("listitem", listitem);
-
-				
-				// 페이지 이동
-				String site = "main.jsp?search=" + search;
-				RequestDispatcher dis = request.getRequestDispatcher(site);
-				dis.forward(request, response);
-				
-				
-				
-			}
-			
-			
-			
-			
-			
-			
-			
-			
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
 
 }
